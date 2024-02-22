81a58e233ddf65cefc2acd6a9a3821efca67ace4
Rajavikas03 <rajavikas00007@gmail.com>
2024-02-22 10:37:45 +0530

    fileref

--- lib/add.dart
+++ lib/add.dart
@@ -20,67 +20,225 @@ class _Add_pageState extends State<Add_page> {
               Gap(20),
               SizedBox(
                 width: 400,
-                child: TextFormField(
-                  style: const TextStyle(color: Colors.white),
-                  cursorColor: Colors.black,
-                  decoration: InputDecoration(
-                    prefixIcon: SizedBox(
-                        width: 30,
-                        height: 40,
-                        child: Image.asset('assets/titleicon.png')),
-                    //  const Icon(
-                    //   Icons.person_outline_outlined,
-                    //   color: Colors.white,
-                    // ),
-                    constraints: const BoxConstraints(maxWidth: 600),
-                    contentPadding: const EdgeInsets.symmetric(
-                        vertical: 0.0, horizontal: 25.0),
-                    hintText: 'Title',
-                    hintStyle:
-                        const TextStyle(fontSize: 17.0, color: Colors.white),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30.0),
-                      borderSide: BorderSide.none,
-                    ),
-                    errorBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30.0),
-                      borderSide: const BorderSide(color: Colors.red),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30.0),
-                      borderSide: const BorderSide(color: Colors.transparent),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30.0),
-                      borderSide: BorderSide.none,
-                    ),
-                    disabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30.0),
-                      borderSide: BorderSide.none,
-                    ),
-                    filled: true,
-                    fillColor:
-                        Colors.grey.withOpacity(0.60), // Background color
-                    errorStyle: const TextStyle(fontSize: 14.0),
-                  ),
-
-                  // Validation for text form//
-
-                  // validator: (value) {
-                  //   if (value == null || value.isEmpty) {
-                  //     return 'Please enter your name';
-                  //   }
-                  //   return null;
-                  // },
-                  // onChanged: (value) {
-                  //   _name = value;
-                  // },
+                child: textformfield(
+                  prefixiicon: Icon(Icons.title),
+                  //  SizedBox(
+                  //   width: 20,
+                  //   height: 20,
+                  //   child: Image.asset('assets/titleicon.png'),
+                  // ),
+                  hinttext: 'Title',
                 ),
               ),
+              Gap(20),
+              SizedBox(
+                width: 400,
+                child: textformfield(
+                  prefixiicon: Icon(Icons.subtitles),
+                  //  SizedBox(
+                  //   width: 20,
+                  //   height: 20,
+                  //   child: Image.asset('assets/titleicon.png'),
+                  // ),
+                  hinttext: 'Sub title',
+                ),
+              ),
+              Gap(20),
+              SizedBox(
+                width: 400,
+                child: textformfield(
+                  prefixiicon: Icon(Icons.domain_add_outlined),
+                  //  SizedBox(
+                  //   width: 20,
+                  //   height: 20,
+                  //   child: Image.asset('assets/titleicon.png'),
+                  // ),
+                  hinttext: 'Domain',
+                ),
+              ),
+              Gap(20),
+              SizedBox(
+                width: 400,
+                child: textformfield(
+                  prefixiicon: Icon(Icons.interests_outlined),
+                  //  SizedBox(
+                  //   width: 20,
+                  //   height: 20,
+                  //   child: Image.asset('assets/titleicon.png'),
+                  // ),
+                  hinttext: 'Aera of intrest',
+                ),
+              ),
+              Gap(20),
+              SizedBox(
+                width: 400,
+                child: textformfield(
+                  prefixiicon: Icon(Icons.title),
+                  //  SizedBox(
+                  //   width: 20,
+                  //   height: 20,
+                  //   child: Image.asset('assets/titleicon.png'),
+                  // ),
+                  hinttext: 'Github link',
+                ),
+              ),
+              Gap(20),
+              TextFormField(
+                maxLines: 3,
+                style: const TextStyle(color: Colors.white),
+                cursorColor: Colors.black,
+                decoration: InputDecoration(
+                  prefixIcon:
+                      // SizedBox(
+                      //   width: 30,
+                      //   height: 40,
+                      //   child:
+                      Icon(
+                    Icons.ac_unit,
+                  ),
+                  // Image.asset('assets/titleicon.png')
+                  // ),
+                  //  const Icon(
+                  //   Icons.person_outline_outlined,
+                  //   color: Colors.white,
+                  // ),
+                  constraints: const BoxConstraints(maxWidth: 600),
+                  contentPadding: const EdgeInsets.symmetric(
+                      vertical: 0.0, horizontal: 25.0),
+                  hintText: 'Description',
+                  //  'Title',
+                  hintStyle:
+                      const TextStyle(fontSize: 17.0, color: Colors.white),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    borderSide: BorderSide.none,
+                  ),
+                  errorBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    borderSide: const BorderSide(color: Colors.red),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    borderSide: const BorderSide(color: Colors.transparent),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    borderSide: BorderSide.none,
+                  ),
+                  disabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    borderSide: BorderSide.none,
+                  ),
+                  filled: true,
+                  fillColor: Colors.grey.withOpacity(0.60), // Background color
+                  errorStyle: const TextStyle(fontSize: 14.0),
+                ),
+
+                // Validation for text form//
+
+                // validator: (value) {
+                //   if (value == null || value.isEmpty) {
+                //     return 'Please enter your name';
+                //   }
+                //   return null;
+                // },
+                // onChanged: (value) {
+                //   _name = value;
+                // },
+              ),
+              Gap(20),
+              SizedBox(
+                width: 400,
+                child: textformfield(
+                  prefixiicon: Icon(Icons.abc_sharp),
+                  //  SizedBox(
+                  //   width: 20,
+                  //   height: 20,
+                  //   child: Image.asset('assets/titleicon.png'),
+                  // ),
+                  hinttext: 'Project level',
+                ),
+              ),
+              Text('Required skills'),
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class textformfield extends StatefulWidget {
+  textformfield({super.key, required this.prefixiicon, required this.hinttext});
+  final Widget prefixiicon;
+
+  final String hinttext;
+
+  @override
+  State<textformfield> createState() => _textformfieldState();
+}
+
+class _textformfieldState extends State<textformfield> {
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      style: const TextStyle(color: Colors.white),
+      cursorColor: Colors.black,
+      decoration: InputDecoration(
+        prefixIcon:
+            // SizedBox(
+            //   width: 30,
+            //   height: 40,
+            //   child:
+            widget.prefixiicon,
+        // Image.asset('assets/titleicon.png')
+        // ),
+        //  const Icon(
+        //   Icons.person_outline_outlined,
+        //   color: Colors.white,
+        // ),
+        constraints: const BoxConstraints(maxWidth: 600),
+        contentPadding:
+            const EdgeInsets.symmetric(vertical: 0.0, horizontal: 25.0),
+        hintText: widget.hinttext,
+        //  'Title',
+        hintStyle: const TextStyle(fontSize: 17.0, color: Colors.white),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30.0),
+          borderSide: BorderSide.none,
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30.0),
+          borderSide: const BorderSide(color: Colors.red),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30.0),
+          borderSide: const BorderSide(color: Colors.transparent),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30.0),
+          borderSide: BorderSide.none,
+        ),
+        disabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30.0),
+          borderSide: BorderSide.none,
+        ),
+        filled: true,
+        fillColor: Colors.grey.withOpacity(0.60), // Background color
+        errorStyle: const TextStyle(fontSize: 14.0),
+      ),
+
+      // Validation for text form//
+
+      // validator: (value) {
+      //   if (value == null || value.isEmpty) {
+      //     return 'Please enter your name';
+      //   }
+      //   return null;
+      // },
+      // onChanged: (value) {
+      //   _name = value;
+      // },
     );
   }
 }
